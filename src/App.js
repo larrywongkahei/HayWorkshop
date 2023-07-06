b833d90afe47b1d0a05d42369ecfd998d95907c8
Maryhei <makemak123@gmail.com>
2023-07-06 20:48:54 +0100

    Added basic css

--- src/App.js
+++ src/App.js
@@ -3,10 +3,39 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
+    <div>
+      <div className='introduction'>
+          <p className='jsonMakerIntroduction'>
+          JsonMaker
+          </p>
+          <p className='hayStoreIntroduction'>
+          HayStore
+          </p>
+          <p className='mentorShipIntroduction'>
+          MentorShip<span> (Coming soon)</span>         
+          </p>
 
-      </header>
+      </div>
+
+    <div className='planetSystem'>
+      {/* <div className='firstLine' />
+      <div className='secondLine' />
+      <div className='thirdLine' /> */}
+      <div className='mainPlanet'>
+        <p>
+          Hay's workshop
+        </p>
+      </div>
+      <div className='jsonPlanet'>
+        <div className='jsonMaker' onClick={() => window.location.href="https://larrywongkahei.github.io/JsonDataMaker/"}/>
+      </div>
+      <div class='hayStorePlanet'>
+      <div className='hayStore' onClick={() => window.location.href="https://haystore.herokuapp.com/"}/>
+      </div>
+      <div class='mentorShipPlanet'>
+        <div className='mentorShip'onClick={() => window.location.href="https://larrywongkahei.github.io/JsonDataMaker/"}/>
+      </div>
+    </div>
     </div>
   );
 }
